f73e310cc38ed355c7199471353f6f68652d45e9
Ionut Vaida <ionut.vaida@pixeldata.ro>
2020-01-28 20:23:34 +0200

    Updates

---
+++ precache-manifest.7a849cfd864a22bfc217b619b0bc0570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1617b2eece5e8709eb3a94213be458d8",
+    "url": "/photo/index.html"
+  },
+  {
+    "revision": "4a37d974ab21192cba0d",
+    "url": "/photo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d0e7f26e807c266d2aea",
+    "url": "/photo/static/js/2.b663be2e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/photo/static/js/2.b663be2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a37d974ab21192cba0d",
+    "url": "/photo/static/js/main.49a8b953.chunk.js"
+  },
+  {
+    "revision": "e9617b26100ee5d9e14a",
+    "url": "/photo/static/js/runtime-main.19360b2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photo/static/media/logo.5d5d9eef.svg"
+  }
+]);
